8054eb699c45735b7bafa881462c721f8e13ea2e
István Juhos <stewemetal@gmail.com>
2021-02-22 08:33:53 +0100

    Add code samples and screenshots to Chapter 3; Commit example project

--- projects/chapters/chapter_03/lib/01_material_app.dart
+++ projects/chapters/chapter_03/lib/01_material_app.dart
@@ -7,6 +7,22 @@ void main() {
 class MaterialDemoApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-Icon()
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        // primaryColor: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          leading: Text("Leading widget"),
+          title: Text("App title"),
+          actions: <Widget>[
+            Text("Action1"),
+            Text("Action2")
+          ],
+        ),
+      ),
+    );
   }
 }
