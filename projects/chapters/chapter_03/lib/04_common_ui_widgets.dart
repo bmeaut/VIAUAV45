61f27348c82ae182ac079a1d673a2d2d5fdd4cbb
István Juhos <stewemetal@gmail.com>
2021-02-22 15:58:31 +0100

    Revert accidental changes in 04_common_ui_widgets.dart

--- projects/chapters/chapter_03/lib/04_common_ui_widgets.dart
+++ projects/chapters/chapter_03/lib/04_common_ui_widgets.dart
@@ -38,87 +38,103 @@ class _EssentialWidgetsDemoPageState extends State<EssentialWidgetsDemoPage> {
       ),
       body: Stack(
         children: <Widget>[
-          Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    Icon(
-                      Icons.android,
-                      color: Colors.greenAccent,
-                      size: 40,
-                    ),
-                    // Icon(
-                    //   Icons.alarm,
-                    //   color: Colors.orangeAccent,
-                    //   size: 40,
-                    // ),
-                  ],
+          Container(
+            margin: EdgeInsets.all(16),
+            padding: EdgeInsets.all(16),
+            decoration: BoxDecoration(
+                shape: BoxShape.rectangle,
+                border: Border.all(
+                  color: Colors.black,
+                  width: 8,
                 ),
-                Image.asset(
-                  'assets/images/logo_flutter.png',
-                  height: 100,
-                ),
-                // SizedBox(
-                //     width: 80,
-                //     height: 20,
-                //     child: Container(
-                //       color: Colors.brown,
-                //     )
-                // ),
-                // Expanded(
-                //   flex: 2,
-                //   child: Image.network(
-                //     "https://picsum.photos/150",
-                //     color: Colors.red,
-                //     colorBlendMode: BlendMode.colorBurn,
-                //     loadingBuilder: (BuildContext context, Widget child,
-                //         ImageChunkEvent? loadingProgress) {
-                //       if (loadingProgress == null) return child;
-                //       return Center(
-                //         child: CircularProgressIndicator(
-                //           value: loadingProgress.expectedTotalBytes != null
-                //               ? loadingProgress.cumulativeBytesLoaded /
-                //                   loadingProgress.expectedTotalBytes!
-                //               : null,
-                //         ),
-                //       );
-                //     },
-                //     headers: {"Authorization": "Bearer 0123456789"},
-                //   ),
-                // ),
-                Text(
-                  'You have pushed the button this many times:',
-                ),
-                Text(
-                  '$_counter',
-                  // style: Theme.of(context).textTheme.headline4,
-                  style: TextStyle(
-                    color: Colors.red,
-                    fontSize: 32,
-                    fontStyle: FontStyle.italic,
-                    fontWeight: FontWeight.bold,
-                    decoration: TextDecoration.underline,
-                    decorationStyle: TextDecorationStyle.dotted,
+                color: Colors.blueGrey,
+                borderRadius: BorderRadius.circular(8)),
+            alignment: Alignment.center,
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      Icon(
+                        Icons.android,
+                        color: Colors.greenAccent,
+                        size: 40,
+                      ),
+                      Icon(
+                        Icons.alarm,
+                        color: Colors.orangeAccent,
+                        size: 40,
+                      ),
+                    ],
                   ),
-                ),
-              ],
+                  Expanded(
+                    flex: 3,
+                    child: Image.asset(
+                      'assets/images/logo_flutter.png',
+                      height: 100,
+                    ),
+                  ),
+                  SizedBox(
+                      width: 80,
+                      height: 20,
+                      child: Container(
+                        color: Colors.brown,
+                      )
+                  ),
+                  Expanded(
+                    flex: 2,
+                    child: Image.network(
+                      "https://picsum.photos/150",
+                      color: Colors.red,
+                      colorBlendMode: BlendMode.colorBurn,
+                      loadingBuilder: (BuildContext context, Widget child,
+                          ImageChunkEvent? loadingProgress) {
+                        if (loadingProgress == null) return child;
+                        return Center(
+                          child: CircularProgressIndicator(
+                            value: loadingProgress.expectedTotalBytes != null
+                                ? loadingProgress.cumulativeBytesLoaded /
+                                    loadingProgress.expectedTotalBytes!
+                                : null,
+                          ),
+                        );
+                      },
+                      headers: {"Authorization": "Bearer 0123456789"},
+                    ),
+                  ),
+                  Text(
+                    'You have pushed the button this many times:',
+                  ),
+                  Text(
+                    '$_counter',
+                    // style: Theme.of(context).textTheme.headline4,
+                    style: TextStyle(
+                      color: Colors.red,
+                      fontSize: 32,
+                      fontStyle: FontStyle.italic,
+                      fontWeight: FontWeight.bold,
+                      decoration: TextDecoration.underline,
+                      decorationStyle: TextDecorationStyle.dotted,
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
-          // Positioned(
-          //   left: 250,
-          //   top: 230,
-          //   child: Image.asset(
-          //     'assets/images/logo_flutter.png',
-          //     height: 100,
-          //   ),
-          // ),
+          Positioned(
+            left: 250,
+            top: 230,
+            child: Image.asset(
+              'assets/images/logo_flutter.png',
+              height: 100,
+            ),
+          ),
         ],
       ),
       // body: Center(
