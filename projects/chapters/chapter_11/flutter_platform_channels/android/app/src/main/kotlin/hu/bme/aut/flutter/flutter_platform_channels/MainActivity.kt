c64b62be878d430ea39c139bc90f09e3c3c307c7
Csongor Vogel <gerfalcon.vogel@gmail.com>
2021-05-02 19:57:28 +0200

    Add basic project to demonstrate the usage of Platform Channels
    Signed-off-by: Csongor Vogel <gerfalcon.vogel@gmail.com>

--- projects/chapters/chapter_11/flutter_platform_channels/android/app/src/main/kotlin/hu/bme/aut/flutter/flutter_platform_channels/MainActivity.kt
+++ projects/chapters/chapter_11/flutter_platform_channels/android/app/src/main/kotlin/hu/bme/aut/flutter/flutter_platform_channels/MainActivity.kt
@@ -1,6 +1,34 @@
 package hu.bme.aut.flutter.flutter_platform_channels
 
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
 class MainActivity : FlutterActivity() {
+
+    private val CHANNEL = "hu.bme.aut.flutter/data"
+
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
+            // Note: this method is invoked on the main thread.
+            if (call.method == "getPlatformSpecificData") {
+                val currentTemp = getPlatformSpecificData()
+                if (currentTemp != "") {
+                    result.success(currentTemp)
+                } else {
+                    result.error("UNAVAILABLE", "Temperature level not available.", null)
+                }
+            } else {
+                result.notImplemented()
+            }
+        }
+    }
+
+    private fun getPlatformSpecificData(): String {
+        val currentTemp = "30"
+        return currentTemp
+    }
+
 }
