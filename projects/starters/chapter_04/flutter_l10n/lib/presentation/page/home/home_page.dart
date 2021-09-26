1fca13944688a6de409f07a80aa4dc3b8dff18f7
Csongor Vogel <gerfalcon.vogel@gmail.com>
2021-09-26 17:30:43 +0200

    add hardcoded string
    Signed-off-by: Csongor Vogel <gerfalcon.vogel@gmail.com>

--- projects/starters/chapter_04/flutter_l10n/lib/presentation/page/home/home_page.dart
+++ projects/starters/chapter_04/flutter_l10n/lib/presentation/page/home/home_page.dart
@@ -10,7 +10,7 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Theme.of(context).primaryColor,
         title: Semantics(
-          child: Text('HomePage'),
+          child: Text('Know your Santa'),
           label: "This is the HomePage.",
         ),
       ),
