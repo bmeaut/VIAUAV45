b7fd076b36b1c7fbd49f436e5a6a16f12f297716
Csongor Vogel <gerfalcon.vogel@gmail.com>
2021-09-26 18:00:41 +0200

    Make the App const

--- projects/starters/chapter_04/flutter_l10n/lib/app.dart
+++ projects/starters/chapter_04/flutter_l10n/lib/app.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 import 'presentation/page/home/home_page.dart';
 
 class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
