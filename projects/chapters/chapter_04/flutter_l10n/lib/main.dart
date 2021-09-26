aea355aeab249f4f0f5acc5aedd1c0424121269a
Csongor Vogel <gerfalcon.vogel@gmail.com>
2021-09-26 23:39:50 +0200

    Add orientation settings
    Signed-off-by: Csongor Vogel <gerfalcon.vogel@gmail.com>

--- projects/chapters/chapter_04/flutter_l10n/lib/main.dart
+++ projects/chapters/chapter_04/flutter_l10n/lib/main.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'app.dart';
 
 void main() {
+  /// TODO - RESPONSIVE UI: Play with the orientations.
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+    DeviceOrientation.landscapeLeft,
+    DeviceOrientation.landscapeRight
+  ]);
   runApp(const App());
 }
