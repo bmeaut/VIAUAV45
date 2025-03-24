cc1d9ea370a7d2e4fac1099f1fafee73db0c043c
Csongor Vogel <vogel.csongor@talabat.com>
2025-03-24 11:20:29 +0400

    Upgrade dependencies and embedders

--- projects/chapters/chapter_07/stock_photo_lists/test/widget_test.dart
+++ projects/chapters/chapter_07/stock_photo_lists/test/widget_test.dart
@@ -7,13 +7,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:stock_photo_lists/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const ListAndGridApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
