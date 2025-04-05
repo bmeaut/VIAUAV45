13044739087700e15390946a812c3df3948439be
Csongor Vogel <vogel.csongor@talabat.com>
2025-04-05 20:31:31 +0400

    Remove unnecessary themedata

--- projects/starters/chapter_08/flutter_http_starter/lib/main.dart
+++ projects/starters/chapter_08/flutter_http_starter/lib/main.dart
@@ -10,9 +10,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
       home: ListPageWidget(),
     );
   }
