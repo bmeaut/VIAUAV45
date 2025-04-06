77d21de998e93713889d48e9f0e65bbf3491f22b
Csongor Vogel <vogel.csongor@talabat.com>
2025-04-06 13:59:05 +0400

    Add todo comments

--- projects/starters/chapter_08/flutter_http/lib/list_repository.dart
+++ projects/starters/chapter_08/flutter_http/lib/list_repository.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_http/list_model.dart';
+import 'package:flutter_http/ow_service.dart';
 
 class ListRepository {
+  /// TODO: 2. Implement the getCities method to fetch real data from the OpenWeather API
+  /// using the [OWService] class.
+
+  // Simulate a network call with a delay
   Future<List<WeatherCityItem>> getCities() async {
     await Future.delayed(Duration(seconds: 2));
     return [
--- projects/starters/chapter_08/flutter_http/lib/ow_service.dart
+++ projects/starters/chapter_08/flutter_http/lib/ow_service.dart
@@ -6,3 +6,9 @@ import 'package:http/http.dart' as http;
 const _openWeatherApiKey = "e6932dac2b4ba21c31ba22d19f4ecc56";
 const _baseUrl = "api.openweathermap.org";
 
+class OWService {
+  /// TODO: 1. Implement OWService with the getOWCities method and parse the response.
+  Future<OWCitiesFindResponse> getOWCities() async {
+    throw UnimplementedError('This method is not yet implemented');
+  }
+}
