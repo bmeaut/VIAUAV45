5094ab6cb093ae2d225f99e4bb22a4704d2a1754
Csongor Vogel <vogel.csongor@talabat.com>
2025-04-06 17:52:24 +0400

    Update flutter dio package

---
+++ projects/chapters/chapter_08/flutter_dio/android/app/src/main/kotlin/hu/bme/aut/example/flutter_dio/MainActivity.kt
@@ -0,0 +1,5 @@
+package hu.bme.aut.example.flutter_dio
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- projects/chapters/chapter_08/flutter_dio/lib/list_repository.dart
+++ projects/chapters/chapter_08/flutter_dio/lib/list_repository.dart
@@ -1,24 +1,27 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter_http/list_model.dart';
-import 'package:flutter_http/ow_service.dart';
-import 'package:weather_icons/weather_icons.dart';
+import 'package:flutter_dio/list_model.dart';
+import 'package:flutter_dio/ow_service.dart';
 
 class ListRepository {
   var owService = OWService();
 
   Future<List<WeatherCityItem>> getCities() async {
     var response = await owService.getOWCities();
-    return response.list.map(
-      (e) => WeatherCityItem(
-        e.id,
-        e.name,
-        NetworkImage("https://openweathermap.org/img/wn/${e.weather[0].icon}.png"),
-        e.main.temp_min,
-        e.main.temp,
-        e.main.temp_max,
-        e.wind.deg,
-        e.wind.speed,
-      ),
-    ).toList();
+    return response.list
+        .map(
+          (e) => WeatherCityItem(
+            e.id,
+            e.name,
+            NetworkImage(
+              "https://openweathermap.org/img/wn/${e.weather[0].icon}.png",
+            ),
+            e.main.temp_min,
+            e.main.temp,
+            e.main.temp_max,
+            e.wind.deg,
+            e.wind.speed,
+          ),
+        )
+        .toList();
   }
 }
