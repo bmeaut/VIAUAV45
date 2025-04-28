c02e9cb7230f480f1df09266bb630c744b803792
Csongor Vogel <vogel.csongor@talabat.com>
2025-04-28 22:32:27 +0400

    Update embedders

--- projects/chapters/chapter_10/flutter_forum/android/app/build.gradle.kts
+++ projects/chapters/chapter_10/flutter_forum/android/app/build.gradle.kts
@@ -1,5 +1,9 @@
 plugins {
     id("com.android.application")
+    // START: FlutterFire Configuration
+    id("com.google.gms.google-services")
+    id("com.google.firebase.crashlytics")
+    // END: FlutterFire Configuration
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
